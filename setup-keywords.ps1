0c5a338a4f68b2ed2617c4032cf24bdde3d308ea
barbatchov <elias.alves.chacon@gmail.com>
2016-02-20 18:21:16 -0200

    Added install for powershell

---
+++ setup-keywords.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [string]$projectPath,
+   [Parameter(Mandatory=$True)]
+   [string]$fileExtensions,
+)
+
+$webclient  =New-Object System.Net.WebClient
+$files =@(
+    https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.smudge.ps1
+    https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.clean.ps1
+)
+$gitConfig  ="$projectPath\.git\config"
+$gitKeywords="$projectPath\.gitkeywords"
+$gitAttributes="$projectPath\.gitattributes"
+
+New-Item -ItemType Directory -Force -Path $gitKeywords
+
+$files | ForEach-Object {
+    $client.DownloadFile( $_, $gitKeywords )
+}
+
+$template=@"[filter `"rcs-keywords`"]
+    smudge= .gitkeywords/rcs.smudge.ps1
+    clean = .gitkeywords/rcs.clean.ps1
+"@
+
+if ( Test-Path $gitAttributes -eq $False ) {
+    Set-Content -Path $gitAttributes -value ""
+}
+
+$fileExtensions.Split(",") | ForEach-Object {
+    Write-Host "$_" >> $gitAttributes
+}
