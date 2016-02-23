1429618a38eb42b9796acecea71df308ac03a5de
Elias Alves Chacon <elias.alves.chacon@gmail.com>
2016-02-23 17:58:41 -0300

    Setup is working now

--- setup-keywords.ps1
+++ setup-keywords.ps1
@@ -1,37 +1,65 @@
 [CmdletBinding()]
 Param(
-   [Parameter(Mandatory=$True,Position=1)]
-   [string]$projectPath,
-   [Parameter(Mandatory=$True)]
-   [string]$fileExtensions,
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$projectPath,
+    [Parameter(Mandatory=$True)]
+    [string]$fileExtensions
 )
 
-$webclient  =New-Object System.Net.WebClient
-$files =@(
-    https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.smudge.ps1
-    https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.clean.ps1
-)
-$gitConfig  ="$projectPath\.git\config"
-$gitKeywords="$projectPath\.gitkeywords"
-$gitAttributes="$projectPath\.gitattributes"
+PROCESS {
+    New-Item -ItemType Directory -Force -Path $gitKeywords
 
-New-Item -ItemType Directory -Force -Path $gitKeywords
-
-$files | ForEach-Object {
-    $client.DownloadFile( $_, $gitKeywords )
-}
-
-$template=@"[filter `"rcs-keywords`"]
+    Write-Host "[1] - Setting up for $projectPath and files with $fileExtensions"
+        [string]  $template         ="[filter `"rcs-keywords`"]
     smudge= .gitkeywords/rcs.smudge.ps1
     clean = .gitkeywords/rcs.clean.ps1
-"@
+"
+        [string]  $gitConfig        ="$projectPath\.git\config"
+        [string]  $gitKeywords      ="$projectPath\.gitkeywords"
+        [string]  $gitAttributes    ="$projectPath\.gitattributes"
+        [string]  $gitAttributesFile=Get-Content $gitAttributes
+        [string[]]$files            =@(
+            "https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.smudge.ps1"
+            "https://raw.githubusercontent.com/barbatchov/powershell-git-keywords/master/rcs.clean.ps1"
+        )
 
-if ( Test-Path $gitAttributes -eq $False ) {
-    Set-Content -Path $gitAttributes -value ""
+    Write-Host "[2] - Downloading files..."
+
+    $files | ForEach-Object {
+        [string]$filename=$_ -replace "^(.*?/)+", ""
+        [string]$location="$gitKeywords\$filename"
+
+        Write-Host "[-] - $_ to $location"
+            [System.Net.WebRequest] $webRequest = [System.Net.WebRequest]::Create($_);
+            [System.Net.WebResponse]$response   = $webRequest.GetResponse();
+            [System.IO.Stream]      $stream     = $response.GetResponseStream();
+            [System.IO.StreamReader]$reader     = New-Object System.IO.StreamReader -argumentList $stream;
+            $reader.ReadToEnd() > $location
+    }
+
+    [string]$config = Get-Content -Path $gitConfig
+    if ( $config -match "rcs-keywords" -eq $True ) {
+        Write-Host "[3] - Already configured..."
+    } else {
+        Write-Host "[3] - Writing configuration..."
+        Write-Host $template >> $gitConfig
+    }
+
+    Write-Host "[4] - Writing git attributes."
+        if ( -not ( Test-Path $gitAttributes ) ) {
+            Write-Host "[-] - Creating new git attributes."
+            Set-Content -Path $gitAttributes -value ""
+        }
+
+        $fileExtensions.Split("-") | ForEach-Object {
+            [string]$ext    = $_
+            [bool]  $exists = $gitAttributesFile -match "\*\.$ext filter\=rcs-keywords" 
+            Write-Host "[-] - $ext - $exists"
+            if ( -not ( $exists ) ) {
+                Write-Host "[-] - Writing $_ to $gitAttributes."
+                Add-Content $gitAttributes "*.$_ filter=rcs-keywords"
+            }
+        }
+
+    Write-Host "[5] - Done"
 }
-
-$fileExtensions.Split(",") | ForEach-Object {
-    Write-Host "$_" >> $gitAttributes
-}
-
-Write-Host $template >> $gitConfig
